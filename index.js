5e2871d7f13cca7889aa49901bb6382cfc88011a
scottbooma <scottbooma@gmail.com>
2022-03-28 14:45:46 -0400

    Add solutions 19+20

--- index.js
+++ index.js
@@ -1,27 +1,33 @@
 const beyonceHash = {
   hits: [
-    { title: 'Bootylicious', group: true, group_name: 'destinysChild', hair: ['straight', 'blonde', 'pink tips'], signature_look: 'fedora with yellow feather', video_theme: 'dressing room dance', best_line: 'Move your body up and down, make your booty touch the ground', fierceness: 7, formation: false, dancers: 20
+    {
+      title: 'Bootylicious', group: true, group_name: 'destinysChild', hair: ['straight', 'blonde', 'pink tips'], signature_look: 'fedora with yellow feather', video_theme: 'dressing room dance', best_line: 'Move your body up and down, make your booty touch the ground', fierceness: 7, formation: false, dancers: 20
     },
-    { title: 'Single Ladies', group: false, group_name: 'none', hair: ['honey brown', 'half up, half down'], signature_look: 'black bodysuit and legs for days', video_theme: 'single ladies dance', best_line: 'if you like it then you should\'ve put a ring on it', fierceness: 10, formation: true, dancers: 2 
+    {
+      title: 'Single Ladies', group: false, group_name: 'none', hair: ['honey brown', 'half up, half down'], signature_look: 'black bodysuit and legs for days', video_theme: 'single ladies dance', best_line: 'if you like it then you should\'ve put a ring on it', fierceness: 10, formation: true, dancers: 2
     },
-    { title: 'Let Me Upgrade You', group: false, group_name: 'none', hair: ['beaded bun', 'wet hair', 'honey brown', 'waves'], signature_look: 'flapper dress and bodysuit', video_theme: 'decadence, water, gold beads and pearls', best_line: 'I could do for you what Martin did for the people', fierceness: 10, formation: true, dancers: 5 
+    {
+      title: 'Let Me Upgrade You', group: false, group_name: 'none', hair: ['beaded bun', 'wet hair', 'honey brown', 'waves'], signature_look: 'flapper dress and bodysuit', video_theme: 'decadence, water, gold beads and pearls', best_line: 'I could do for you what Martin did for the people', fierceness: 10, formation: true, dancers: 5
     },
-    { title: 'Sorry', group: false, group_name: 'none', hair: ['braids', 'bun', 'blonde', 'waves', 'bob'], signature_look: 'braided crown with gold bikini top', video_theme: 'party bus and Serena Williams in bodysuit', best_line: 'Boi bye', fierceness: 10, formation: false, dancers: 1 
+    {
+      title: 'Sorry', group: false, group_name: 'none', hair: ['braids', 'bun', 'blonde', 'waves', 'bob'], signature_look: 'braided crown with gold bikini top', video_theme: 'party bus and Serena Williams in bodysuit', best_line: 'Boi bye', fierceness: 10, formation: false, dancers: 1
     },
-    { title: 'Say My Name', group: true, group_name: 'destinysChild', hair: ['blonde', 'curly', 'braid', 'pony tail'], signature_look: 'none', video_theme: 'color blocked scenes with group', best_line: 'say my name, say my name', fierceness: 7, formation: true, dancers: 6 
+    {
+      title: 'Say My Name', group: true, group_name: 'destinysChild', hair: ['blonde', 'curly', 'braid', 'pony tail'], signature_look: 'none', video_theme: 'color blocked scenes with group', best_line: 'say my name, say my name', fierceness: 7, formation: true, dancers: 6
     },
-    { title: 'Feeling Myself', group: true, group_name: 'Nicki Minaj featuring Beyonce', hair: ['blonde', 'waves'], signature_look: 'sporty bodysuit', video_theme: 'Coachella music festival', best_line: 'Im felling myself', fierceness: 9, formation: false, dancers: 0 
+    {
+      title: 'Feeling Myself', group: true, group_name: 'Nicki Minaj featuring Beyonce', hair: ['blonde', 'waves'], signature_look: 'sporty bodysuit', video_theme: 'Coachella music festival', best_line: 'Im felling myself', fierceness: 9, formation: false, dancers: 0
     }
   ],
   movies: [
-    { title: 'Austin Power\'s Goldmember', year: 2002, rating: 5},
-    { title: 'Dreamgirls', year: 2006, rating: 7},
-    { title: 'Obsessed', year: 2009, rating: 6},
-    { title: 'Cadillac Records', year: 2008, rating: 8},
-    { title: 'Life is But a Dream', year: 2013, rating: 6},
-    { title: 'The Pink Panther', year: 2006, rating: 4},
-    { title: 'Epic', year: 2013, rating: 7},
-    { title: 'The Fighting Temptations', year: 2003, rating: 5}
+    { title: 'Austin Power\'s Goldmember', year: 2002, rating: 5 },
+    { title: 'Dreamgirls', year: 2006, rating: 7 },
+    { title: 'Obsessed', year: 2009, rating: 6 },
+    { title: 'Cadillac Records', year: 2008, rating: 8 },
+    { title: 'Life is But a Dream', year: 2013, rating: 6 },
+    { title: 'The Pink Panther', year: 2006, rating: 4 },
+    { title: 'Epic', year: 2013, rating: 7 },
+    { title: 'The Fighting Temptations', year: 2003, rating: 5 }
   ]
 };
 
@@ -87,19 +93,19 @@ function fiercestHits() {
 
 // 13. Return the sum of Beyonce's fierceness value for all of her hit songs
 function hitFiercenessSum() {
-  return allFiercenessRatings().reduce((previousFierceness, currentFierceness) => previousFierceness + currentFierceness, 
-  0)
+  return allFiercenessRatings().reduce((previousFierceness, currentFierceness) => previousFierceness + currentFierceness,
+    0)
 }
 
 // 14. Return the average fierceness value for all Beyonce's hit songs
 function hitFiercenessAverage() {
-return hitFiercenessSum() / allFiercenessRatings().length
+  return hitFiercenessSum() / allFiercenessRatings().length
 }
 
 // 15. Return the sum of Beyonce's rating value for all of her movies
 function ratingSum() {
   return beyonceHash.movies.map(movie => movie.rating).reduce((previousRating, currentRating) => previousRating + currentRating,
-  0)
+    0)
 }
 
 // 16. Return the average rating value for all of her movies
@@ -110,22 +116,43 @@ function ratingAverage() {
 // 17. Return the sum of the total number of dancers in all of the hit song videos
 function hitDancerSum() {
   return beyonceHash.hits.map(hit => hit.dancers).reduce((previousDancer, currentDancer) => previousDancer + currentDancer,
-  0)
+    0)
 }
 
 // 18. Return an array of Beyonce's hairstyles without repeats
 function uniqueHairstyles() {
-  const hairstyles = beyonceHash.hits.map(hit => hit.hair).reduce((previousHairstyle, currentHairstyle) => previousHairstyle.concat(currentHairstyle),
-  [])
-  return [...new Set(hairstyles)]
+  return [...new Set(beyonceHash.hits.map(hit => hit.hair).reduce((previousHairstyle, currentHairstyle) => previousHairstyle.concat(currentHairstyle),
+    []))]
 }
 
 // 19. Return an object where the properties are song names and the value is an object which contains that song's fierceness and the average fierceness for all songs
 function songFiercenessByName() {
+  let object = {}
+  for(let hit of beyonceHash.hits) {
+    object = {
+      ...object,
+      [hit.title]: {
+        fierceness: hit.fierceness,
+        averageFierceness: hitFiercenessAverage(),
+      }
+    }
+  }
+  return object
 }
 
 // 20. Return an object where the properties are movie names and the value is an object which contains that movie's rating and the average rating for all movies
 function movieRatingsByName() {
+  let object = {}
+  for(let movie of beyonceHash.movies) {
+    object = {
+      ...object,
+      [movie.title]: {
+        rating: movie.rating,
+        averageRating: ratingAverage(),
+      }
+    }
+  }
+  return object
 }
 
 // 21. Return an object with Beyonce's hairstyles as the keys and a tally of each hairstyle, eg. `{ "blonde": 3, ... }`
@@ -189,3 +216,9 @@ function hairStyleFrequency() {
 
 // 18.
 // console.log(uniqueHairstyles())
+
+// 19.
+// console.log(songFiercenessByName())
+
+// 20.
+// console.log(movieRatingsByName())
